2649a4fc4ad709ed50e70208d4d1ed00713b8c21
zen815 <zen815@gmail.com>
2015-06-11 14:45:31 +0900

    link path

--- server.js
+++ server.js
@@ -4,8 +4,14 @@
 var connect = require('connect'),
     serveStatic = require('serve-static');
 
-var app = connect;
+var app = connect();
 
-app.use(serveStatic("../03.example_ch9"));
+app.use(serveStatic("../03.example_ch9/src"));
+
+app.use('/css', serveStatic(__dirname + '/node_modules/bootstrap/dist/css/'));
+
+app.use('/scripts', serveStatic(__dirname + '/node_modules/angular/'));
 
 app.listen(5000);
+
+console.log("localhost:5000");
